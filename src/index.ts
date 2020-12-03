288b36be1ec1035c0065dad017dc20bddc7544bc
Sujan <pokharelsujan134@gmail.com>
2020-12-03 12:53:28 +0200

    the simple solution with a sort of Plain JS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hi there")
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {encoding:'utf-8'})
+.split('\n')
+.map ( (row:string):string[]=> {
+    return row.split(',')
+})
+
+
+// Enem- enumeration => object that stores closely related values
+const MatchResult = {
+    HomeWin: 'H',
+    AwayWin: "A",
+    Draw: 'D'
+}
+
+let manUtdWins = 0;
+for ( let match of matches ){
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin){
+        manUtdWins++;
+    }else if (match[2]==='Man United' && match[5] === MatchResult.AwayWin){
+        manUtdWins++;
+    }
+}
+;
+console.log(`Man United won ${manUtdWins} games`)
